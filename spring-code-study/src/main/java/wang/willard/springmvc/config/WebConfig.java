0697410c71843824e1d21f660397aad626a22e92
wang.zhongyi <meddkim@163.com>
2018-02-28 15:59:03 +0800

    fix

--- spring-code-study/src/main/java/wang/willard/springmvc/config/WebConfig.java
+++ spring-code-study/src/main/java/wang/willard/springmvc/config/WebConfig.java
@@ -1,11 +1,8 @@
 package wang.willard.springmvc.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.JstlView;
-import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @Configuration
 @ComponentScan("wang.willard.springmvc")
