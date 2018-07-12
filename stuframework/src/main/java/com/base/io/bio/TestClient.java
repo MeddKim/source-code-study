e5639534920f65286c207011540aab61bf973311
unknown <meddkim@163.com>
2018-07-12 19:02:08 +0800

    fix

---
+++ stuframework/src/main/java/com/base/io/bio/TestClient.java
@@ -0,0 +1,21 @@
+package com.base.io.bio;
+
+import java.util.Scanner;
+
+public class TestClient {
+    public static void main(String[] args) {
+//        BIOClient.send("你好");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextLine());
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println(scanner.nextLine());
+            }
+        });
+
+        thread.start();
+    }
+}
